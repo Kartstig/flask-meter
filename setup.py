593d64cae42b3d02d54ac2609ca6e331937e3dcf
Herman Singh <kartstig@gmail.com>
2017-02-01 00:38:57 -0500

    Add requirements and deps

--- setup.py
+++ setup.py
@@ -10,12 +10,11 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'Click>=6.0',
-    # TODO: put package requirements here
+    'Flask',
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'pytest'
 ]
 
 setup(
