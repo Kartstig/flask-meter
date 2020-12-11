aa2338ee823f9eb01e9078565e8527965e8d468e
Herman Singh <kartstig@gmail.com>
2020-12-11 11:14:46 -0500

    Formatting with autopep8

--- flask_meter/git.py
+++ flask_meter/git.py
@@ -9,14 +9,14 @@ RE_DATE = re.compile(r'Date\:[\W]+(.*)', re.MULTILINE)
 
 
 def git_stats():
-  try:
-    ret = subprocess.check_output([GIT_CMD], shell=True)\
-                    .decode('utf-8')
-    commit = re.search(RE_COMMIT, ret)
-    author = re.search(RE_AUTHOR, ret)
-    date = re.search(RE_DATE, ret)
-    return {"commit": commit.group(1) if commit else "Unknown",
-            "author": author.group(1) if author else "Unknown",
-            "date": date.group(1) if date else "Unknown"}
-  except subprocess.CalledProcessError:
-    return {"Error"}
+    try:
+        ret = subprocess.check_output([GIT_CMD], shell=True)\
+                        .decode('utf-8')
+        commit = re.search(RE_COMMIT, ret)
+        author = re.search(RE_AUTHOR, ret)
+        date = re.search(RE_DATE, ret)
+        return {"commit": commit.group(1) if commit else "Unknown",
+                "author": author.group(1) if author else "Unknown",
+                "date": date.group(1) if date else "Unknown"}
+    except subprocess.CalledProcessError:
+        return {"Error"}
